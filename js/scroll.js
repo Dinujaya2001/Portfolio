e093f46afb3c9a3aeb633eae0dfe5d2de50a56bb
Dinujaya2001 <dinujayatharaka2001@gmail.com>
2026-08-11 09:36:04 +0530

    feat: add index.html with personalized details for W.H.D. Tharaka and Software Engineer profile

--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrolled = (winScroll / height) * 100;
-    const progressBar = document.getElementById('scroll-progress');
-    if (progressBar) {
-        progressBar.style.width = scrolled + '%';
-    }
-});
